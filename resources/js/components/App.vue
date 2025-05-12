9c1826c2cc5f7bcc4b5f5db5b9b736993c08262d
namle118205 <nle61144@gmail.com>
2025-05-12 09:47:24 +0700

    sửa lại

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -4,7 +4,7 @@
       <nav>
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
-        <router-link to="/products">Products</router-link>
+        <router-link to="/products">Product</router-link>
       </nav>
       <router-view></router-view> <!-- Nơi hiển thị các component -->
     </div>
