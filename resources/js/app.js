d031aca273d12a13997d854e00226ae581e174a2
namle118205 <nle61144@gmail.com>
2025-05-19 15:30:14 +0700

    đưa code lên

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,7 @@ import ReservationComponent from './components/ReservationComponent.vue';
 import BlogComponent from './components/BlogComponent.vue';
 import MenuComponent from './components/MenuComponent.vue';
 import BlogDetailComponent from './components/BlogDetailComponent.vue';
+import AdminDashboardComponent from './components/AdminDashboardComponent.vue';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
@@ -24,6 +25,7 @@ const routes = [
     { path: '/blog-detail', component: BlogDetailComponent },
     { path: '/contact', component: ContactComponent },
     { path: '/reservation', component: ReservationComponent },
+    { path: '/admin-dashboard', component: AdminDashboardComponent },
 ];
 
 const router = createRouter({
