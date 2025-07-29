91b042ce4904a35364795dc634fc39ee29f37ab2
Thọ Lê Minh <tho@example.com>
2025-07-29 13:40:57 +0700

    upcode

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        date_default_timezone_set('Asia/Ho_Chi_Minh'); // Đặt múi giờ PHP
+        Carbon::setLocale('vi'); // Nếu muốn Carbon trả về tháng/ngày theo tiếng Việt
     }
 }
