df5686ee874800766ab92ceadc11d0f7ad3af589
namle118205 <nle61144@gmail.com>
2025-08-19 21:32:04 +0700

    up code

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -94,8 +94,4 @@ class User extends Authenticatable implements JWTSubject
         return $this->hasOne(StaffProfile::class, 'user_id', 'id');
     }
 
-    public function staffSchedules()
-    {
-        return $this->hasMany(StaffSchedule::class, 'user_id', 'id');
-    }
 }
